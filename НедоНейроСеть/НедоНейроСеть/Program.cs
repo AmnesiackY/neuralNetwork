a25be9e9bd9dd812010624cbc94e26e1830c7832
AmnesiackY <hitsac2711@gmail.com>
2021-12-11 16:48:37 +0200

    Clean connected project

--- НедоНейроСеть/НедоНейроСеть/Program.cs
+++ НедоНейроСеть/НедоНейроСеть/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml;
+using static System.Math;
+using static System.Console;
+enum MemoryMode { GET, SET }
+enum NeuronType { Hidden, Output}
+
+namespace NeuralNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
