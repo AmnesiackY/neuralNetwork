a1715e7b2dd60f07663b56788fe18cf8622886e0
AmnesiackY <hitsac2711@gmail.com>
2021-12-11 16:53:45 +0200

    add class for Training set

---
+++ НедоНейроСеть/НедоНейроСеть/InputLayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace НедоНейроСеть
+{
+    class InputLayer
+    {
+        private (double[], double[])[] _trainset = new (double[], double[])[]
+        {
+            (new double[]{0, 0}, new double[]{0, 1}),
+            (new double[]{0, 0}, new double[] { 0, 1 }),
+            (new double[]{ 0, 0 }, new double[] { 0, 1 }),
+            (new double[]{ 0, 0 }, new double[] { 0, 1 }),
+        };
+        public (double[], double[])[] Trainset {  get => _trainset; }
+    }
+}
